7b259db8c7d53c22cba689241b4ace8447a7474e
Hector Rios <103393059+Hrios05@users.noreply.github.com>
2025-04-04 16:51:39 -0700

    StarterCode Updated

---
+++ EquiBot/EquiBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquiBotApp.swift
+//  EquiBot
+//
+//  Created by Hector Rios on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct EquiBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
