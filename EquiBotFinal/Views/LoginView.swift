c4e7aa8d078202a63a87534ea5d8960778dae431
LJennifer21 <lucerojennifer607@gmail.com>
2025-04-05 05:07:48 -0700

    loginpage

--- EquiBotFinal/Views/LoginView.swift
+++ EquiBotFinal/Views/LoginView.swift
@@ -18,7 +18,7 @@ struct LoginView: View {
     var body: some View {
         
         ZStack{
-            Text("Sign in").font(Font.custom("Poppins-SemiBold", size: 50)).fontWeight(.bold)
+            Text("Sign in").font(                  Font.custom("Poppins-SemiBold", size: 50)).fontWeight(.bold)
                 .position(x:200, y:175)
             
             Color(red: 0.1098, green: 0.1647, blue:  0.3020)
@@ -27,13 +27,7 @@ struct LoginView: View {
                 .frame(width: 280, height: 75)
                 .position(x:200, y:400)
             
-            Color(red: 207/255.0, green: 223/255.0, blue:  223/255.0)
-            
-                .cornerRadius(20.0)
-                .padding(.all)
-                .frame(width: 287, height: 75)
-                .position(x:200, y:-30)
-            
+        
             
             
             
@@ -55,7 +49,7 @@ struct LoginView: View {
                  .cornerRadius(20.0)
                  .padding(.all)
                  .frame(width: 287, height: 75)
-                 .position(x:200, y:-15)*/
+                 .position(x:200, y:-15)
                 
                 
                 Color(red: 207/255.0, green: 223/255.0, blue:  223/255.0)
@@ -63,7 +57,7 @@ struct LoginView: View {
                     .cornerRadius(20.0)
                     .padding(.all)
                     .frame(width: 287, height: 75)
-                    .position(x:200, y:-220)
+                    .position(x:200, y:-220)*/
                 
                 
                 
@@ -76,16 +70,48 @@ struct LoginView: View {
                     .cornerRadius(20.0)
                     .padding(.all)
                     .frame(width: 287, height: 75)
-                    .position(x:200, y:-150)
+                    .position(x:200, y:0)
                 VStack{
                     Text("Username")
-                        .position(x:200, y:-150)
+                        .position(x:130, y:0)
                     //Color(.white)
                         
                         .foregroundColor(.black)
                     
                 }
             }
+        ZStack{
+            Color(red: 207/255.0, green: 223/255.0, blue:  223/255.0)
+            
+                .cornerRadius(20.0)
+                .padding(.all)
+                .frame(width: 287, height: 75)
+                .position(x:200, y:-200)
+            VStack{
+                Text("Pasword")
+                    .position(x:125, y:-200)
+                //Color(.white)
+                    
+                    .foregroundColor(.black)
+                
+                Text("Don't have an account?").font(                  Font.custom("Poppins-SemiBold", size: 15))
+                    .position(x:160, y:-100)
+                Text("Register").font(                  Font.custom("Poppins-SemiBold", size: 15)).fontWeight(.bold)
+                    .position(x:300, y:-200)
+                
+                
+                Image("Logo").resizable()
+                    .imageScale(.large).frame(width: 200, height: 200)
+                    .padding(.leading, 40)
+                    .foregroundStyle(.tint)
+                    .position(x:185, y:-50)
+                Text("EquiBot").font(Font.custom("Poppins-SemiBold", size: 50)).fontWeight(.bold)
+                
+            }
+            VStack{
+                
+            }
+        }
             
         }
     }
